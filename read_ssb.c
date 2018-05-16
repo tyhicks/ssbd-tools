c40f4610e0ed6366916fbeb8ff7d184e4de10839
Tyler Hicks <tyhicks@canonical.com>
2018-05-16 19:24:53 +0000

    Verify the SSBD bit's value in the SPEC_CTRL MSR
    Note that this adds the requirement to have root privileges to read from
    the /dev/cpu/*/msr files.
    
    Signed-off-by: Tyler Hicks <tyhicks@canonical.com>

--- read_ssb.c
+++ read_ssb.c
@@ -19,6 +19,8 @@
 
 #define _GNU_SOURCE
 #include <errno.h>
+#include <fcntl.h>
+#include <inttypes.h>
 #include <sched.h>
 #include <stddef.h>
 #include <stdio.h>
@@ -30,7 +32,9 @@
 #include <linux/audit.h>
 #include <sys/ptrace.h>
 #include <sys/prctl.h>
+#include <sys/stat.h>
 #include <sys/syscall.h>
+#include <sys/types.h>
 
 #ifndef PR_GET_SPECULATION_CTRL
 #define PR_GET_SPECULATION_CTRL 52
@@ -50,6 +54,68 @@
 # define PR_SPEC_ENABLE		(1UL << 1)
 # define PR_SPEC_DISABLE	(1UL << 2)
 # define PR_SPEC_FORCE_DISABLE	(1UL << 3)
+
+#define IA32_SPEC_CTRL_MSR	0x48
+
+int verify_prctl(int cpu, int ssbd)
+{
+	char msr_path[64];
+	uint64_t value;
+	int msr_fd;
+	int rc;
+
+	rc = snprintf(msr_path, sizeof(msr_path), "/dev/cpu/%d/msr", cpu);
+	if (rc < 0 || rc >= sizeof(msr_path) ){
+		fprintf(stderr, "%s: Failed to construct MSR path", __func__);
+		return -1;
+	}
+
+	msr_fd = open(msr_path, O_RDONLY | O_CLOEXEC);
+	if (msr_fd < 0) {
+		if (errno == ENOENT) {
+			fprintf(stderr, "Please load the msr module and try again\n");
+		} else if (errno == EACCES) {
+			fprintf(stderr, "WARNING: Skipping verification of the SPEC_CTRL MSR; run as root to perform verification\n");
+			return 0;
+		} else {
+			perror("open");
+		}
+		return -1;
+	}
+
+	rc = pread(msr_fd, &value, sizeof(value), IA32_SPEC_CTRL_MSR);
+	if (rc < 0) {
+		perror("pread");
+		return -1;
+	} else if (rc != sizeof(value)) {
+		fprintf(stderr, "%s: short read of the MSR\n", __func__);
+		return -1;
+	}
+
+	switch (ssbd) {
+	case PR_SPEC_NOT_AFFECTED:
+	case PR_SPEC_PRCTL | PR_SPEC_ENABLE:
+		if (value & 0x4) {
+			fprintf(stderr, "Bit 2 of IA32_SPEC_CTRL MSR is unexpectedly set");
+			return -1;
+		}
+		break;
+	case PR_SPEC_PRCTL | PR_SPEC_DISABLE:
+	case PR_SPEC_PRCTL | PR_SPEC_FORCE_DISABLE:
+	case PR_SPEC_DISABLE:
+		if (!(value & 0x4)) {
+			fprintf(stderr, "Bit 2 of IA32_SPEC_CTRL MSR is unexpectedly clear");
+			return -1;
+		}
+		break;
+	default:
+		fprintf(stderr,
+			"Unknown SSBD status (0x%x); can't verify MSR\n", ssbd);
+		return -1;
+	}
+
+	return 0;
+}
 
 int get_prctl(void)
 {
@@ -202,6 +268,8 @@ int main(int argc, char **argv)
 		exit(1);
 
 	print_prctl(ssbd);
+	if (verify_prctl(0, ssbd) < 0)
+		exit(1);
 
 	exit(0);
 }
