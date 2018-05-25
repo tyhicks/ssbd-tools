dcbbf36db682599894dc068596ae685b48edad9a
Tyler Hicks <tyhicks@canonical.com>
2018-05-25 18:03:02 +0000

    Automatically detect SSBD support in processor
    Use cpuid and rdmsr to identify if the processor supports SSBD and is
    even affected by SSB to determine what needs to be done to test SSBD.
    
    Signed-off-by: Tyler Hicks <tyhicks@canonical.com>

--- check_ssbd.c
+++ check_ssbd.c
@@ -18,6 +18,7 @@
  */
 
 #define _GNU_SOURCE
+#include <cpuid.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <inttypes.h>
@@ -78,6 +79,7 @@
 #endif
 
 #define IA32_SPEC_CTRL_MSR		0x48
+#define IA32_ARCH_CAPABILITIES_MSR	0x10A
 #define AMD64_VIRT_SPEC_CTRL_MSR	0xc001011f
 #define AMD64_LS_CFG_MSR		0xc0011020
 
@@ -88,6 +90,8 @@ typedef enum { CPU_INTEL,
 	       CPU_AMD_15H,
 	       CPU_AMD_16H,
 	       CPU_AMD_17H,
+	       CPU_SSB_UNAFFECTED,
+	       CPU_SSBD_UNSUPPORTED,
 	      } cpu_id;
 
 /* Waits for the child to exit and exits with the same return value
@@ -151,6 +155,26 @@ int open_msr_fd(int cpu_num)
 	return msr_fd;
 }
 
+/* Read the value from the MSR
+ *
+ * Returns 0 on success. -1 on error.
+ */
+int read_msr(uint64_t *value, int msr_fd, off_t msr)
+{
+	int rc;
+
+	rc = pread(msr_fd, value, sizeof(*value), msr);
+	if (rc < 0) {
+		fprintf(stderr, "ERROR: Couldn't read MSR file: %m\n");
+		return -1;
+	} else if (rc != sizeof(value)) {
+		fprintf(stderr, "ERROR: Short read of the MSR file\n");
+		return -1;
+	}
+
+	return 0;
+}
+
 /* Read the SSBD bit from the MSR corresponding to cpu_id
  *
  * Sets *ssbd to true if the bit is 1, false if the bit is 0.
@@ -161,7 +185,6 @@ int read_ssbd_from_msr(int msr_fd, cpu_id cpu_id, bool *ssbd)
 {
 	uint64_t mask, value;
 	off_t msr;
-	int rc;
 
 	switch (cpu_id) {
 	case CPU_AMD_VIRT:
@@ -186,14 +209,8 @@ int read_ssbd_from_msr(int msr_fd, cpu_id cpu_id, bool *ssbd)
 		mask = 1ULL << 2;
 	}
 
-	rc = pread(msr_fd, &value, sizeof(value), msr);
-	if (rc < 0) {
-		fprintf(stderr, "ERROR: Couldn't read MSR file: %m\n");
+	if (read_msr(&value, msr_fd, msr))
 		return -1;
-	} else if (rc != sizeof(value)) {
-		fprintf(stderr, "ERROR: Short read of the MSR file\n");
-		return -1;
-	}
 
 	*ssbd = !!(value & mask);
 	return 0;
@@ -427,6 +444,118 @@ int load_seccomp_filter(unsigned int flags)
 	return 0;
 }
 
+/* Sets cpu_id in accordance to SSBD support of the current Intel processor
+ *
+ * Returns 0 on success. -1 on error.
+ */
+int identify_intel_cpu(cpu_id *cpu_id, int msr_fd)
+{
+	uint32_t eax, ebx, ecx, edx;
+
+	__cpuid_count(0x7, 0, eax, ebx, ecx, edx);
+
+	if (!(edx & (1ULL << 31))) {
+		*cpu_id = CPU_SSBD_UNSUPPORTED;
+		return 0;
+	} else if (edx & (1ULL << 29)) {
+		uint64_t value;
+
+		if (read_msr(&value, msr_fd, IA32_ARCH_CAPABILITIES_MSR))
+			return -1;
+
+		if (value & (1ULL << 4)) {
+			*cpu_id = CPU_SSB_UNAFFECTED;
+			return 0;
+		}
+	}
+
+	*cpu_id = CPU_INTEL;
+	return 0;
+}
+
+/* Sets cpu_id in accordance to SSBD support of the current AMD processor
+ *
+ * Returns 0 on success. -1 on error.
+ */
+int identify_amd_cpu(cpu_id *cpu_id)
+{
+	uint32_t eax, ebx, ecx, edx;
+
+	__cpuid(0x80000008, eax, ebx, ecx, edx);
+
+	if (ebx & (1ULL << 26)) {
+		*cpu_id = CPU_SSB_UNAFFECTED;
+	} else if (ebx & (1ULL << 24)) {
+		*cpu_id = CPU_INTEL;
+	} else if (ebx & (1ULL << 25)) {
+		*cpu_id = CPU_AMD_VIRT;
+	} else {
+		uint32_t base, extended, family;
+
+		__cpuid(0x1, eax, ebx, ecx, edx);
+		base = (eax >> 8) & 0xF;
+		extended = (eax >> 20) & 0xFF;
+
+		if (base < 0xF) {
+			fprintf(stderr, "ERROR: Unsupported AMD family: 0x%x\n",
+				base);
+			return -1;
+		}
+
+		family = base + extended;
+		switch (family) {
+		case 0x15:
+			*cpu_id = CPU_AMD_15H;
+			break;
+		case 0x16:
+			*cpu_id = CPU_AMD_16H;
+			break;
+		case 0x17:
+			*cpu_id = CPU_AMD_17H;
+			break;
+		default:
+			fprintf(stderr, "ERROR: Unsupported AMD family: 0x%x\n",
+				family);
+			return -1;
+		}
+	}
+
+	return 0;
+}
+
+void uint32_to_string(uint32_t i, char s[4])
+{
+	s[0] = i & 0xFF;
+	s[1] = i >> 8 & 0xFF;
+	s[2] = i >> 16 & 0xFF;
+	s[3] = i >> 24;
+}
+
+/* Sets cpu_id in accordance to SSBD support of the current processor
+ *
+ * Returns 0 on success. -1 on error.
+ */
+int identify_cpu(cpu_id *cpu_id, int msr_fd)
+{
+	uint32_t eax, ebx, ecx, edx;
+	char vendor[13];
+
+	__cpuid(0, eax, ebx, ecx, edx);
+	uint32_to_string(ebx, vendor);
+	uint32_to_string(edx, vendor + 4);
+	uint32_to_string(ecx, vendor + 8);
+	vendor[12] = 0;
+
+	if (!strcmp(vendor, "GenuineIntel")) {
+		return identify_intel_cpu(cpu_id, msr_fd);
+	} else if (!strcmp(vendor, "AuthenticAMD")) {
+		return identify_amd_cpu(cpu_id);
+	} else {
+		fprintf(stderr, "ERROR: Unsupported CPU vendor: %s\n", vendor);
+		return 1;
+	}
+}
+
 /* Restricts the current process to only run on the specified CPU
  *
  * Returns 0 on success. -1 on error.
@@ -451,12 +580,6 @@ int usage(const char *prog)
 	fprintf(stderr,
 		"Usage: %s [options] [-- ...]\n\n"
 		"Valid options are:\n"
-		"  -C CPUID      An identifier to describe the CPU that's being used.\n"
-		"                The default is \"intel\". Other valid values are:\n"
-		"                 \"amd-virt\" for AMD virtual speculation controls\n"
-		"                 \"amd-15h\" for AMD Family 15h processors\n"
-		"                 \"amd-16h\" for AMD Family 16h processors\n"
-		"                 \"amd-17h\" for AMD Family 17h processors\n"
 		"  -c CPUNUM     Pin the process to the CPUNUM cpu. The default is 0.\n"
 		"  -q            Don't print the string represenation of the prctl value\n"
 		"  -p VALUE      Use PR_SET_SPECULATION_CTRL with the specified value. Valid\n"
@@ -501,7 +624,6 @@ struct options {
 	const char *exec;	/* Program to exec */
 	char **exec_argv;	/* Arguments to pass to program */
 
-	cpu_id cpu_id;		/* CPU identifier */
 	int cpu_num;		/* CPU number to restrict the process to */
 
 	bool quiet;		/* Whether to print the prctl value */
@@ -517,26 +639,11 @@ void parse_opts(int argc, char **argv, struct options *opts)
 	opts->seconds = (time_t) -1;
 	opts->fork = true;
 	opts->cpu_num = DEFAULT_CPU_NUM;
-	opts->cpu_id = CPU_INTEL;
 
-	while ((o = getopt(argc, argv, "C:c:e:np:qs:")) != -1) {
+	while ((o = getopt(argc, argv, "c:e:np:qs:")) != -1) {
 		char *secs = NULL;
 
 		switch(o) {
-		case 'C': /* CPU identifier */
-			if (!strcmp(optarg, "intel"))
-				opts->cpu_id = CPU_INTEL;
-			else if (!strcmp(optarg, "amd-virt"))
-				opts->cpu_id = CPU_AMD_VIRT;
-			else if (!strcmp(optarg, "amd-15h"))
-				opts->cpu_id = CPU_AMD_15H;
-			else if (!strcmp(optarg, "amd-16h"))
-				opts->cpu_id = CPU_AMD_16H;
-			else if (!strcmp(optarg, "amd-17h"))
-				opts->cpu_id = CPU_AMD_17H;
-			else
-				usage(prog);
-			break;
 		case 'c': /* CPU number */
 			opts->cpu_num = atoi(optarg);
 			break;
@@ -607,6 +714,7 @@ int main(int argc, char **argv)
 {
 	struct options opts;
 	int msr_fd;
+	cpu_id cpu_id;
 	int prctl_value;
 	pid_t pid;
 
@@ -618,6 +726,17 @@ int main(int argc, char **argv)
 	msr_fd = open_msr_fd(opts.cpu_num);
 	if (msr_fd < 0)
 		exit(EXIT_FAILURE);
+
+	if (identify_cpu(&cpu_id, msr_fd))
+		exit(EXIT_FAILURE);
+
+	if (cpu_id == CPU_SSBD_UNSUPPORTED) {
+		fprintf(stderr, "FAIL: SSBD is unsupported by this CPU\n");
+		exit(EXIT_FAILURE);
+	} else if (cpu_id == CPU_SSB_UNAFFECTED) {
+		printf("This CPU is not affected by Speculative Store Bypass\n");
+		exit(EXIT_SUCCESS);
+	}
 
 	if (opts.prctl && set_prctl(opts.prctl_value))
 		exit(EXIT_FAILURE);
@@ -633,24 +752,24 @@ int main(int argc, char **argv)
 		print_prctl(prctl_value);
 
 	/* Verify that the returned prctl value matches with the MSR */
-	if (opts.verify_ssbd && verify_prctl(msr_fd, opts.cpu_id, prctl_value))
+	if (opts.verify_ssbd && verify_prctl(msr_fd, cpu_id, prctl_value))
 		exit(EXIT_FAILURE);
 
 	if (opts.exec && opts.fork) {
 		/* Do a single SSBD verification prior to forking */
 		if (opts.verify_ssbd &&
-		    verify_ssbd(msr_fd, opts.cpu_id, opts.ssbd, (time_t) -1))
+		    verify_ssbd(msr_fd, cpu_id, opts.ssbd, (time_t) -1))
 			exit(EXIT_FAILURE);
 
 		/* This will do a single SSBD verification after forking */
-		pid = fork_verify_exec(opts.verify_ssbd, msr_fd, opts.cpu_id,
+		pid = fork_verify_exec(opts.verify_ssbd, msr_fd, cpu_id,
 				       opts.ssbd, opts.exec, opts.exec_argv);
 		if (pid < 0)
 			exit(EXIT_FAILURE);
 	}
 
 	if (opts.verify_ssbd &&
-	    verify_ssbd(msr_fd, opts.cpu_id, opts.ssbd, opts.seconds))
+	    verify_ssbd(msr_fd, cpu_id, opts.ssbd, opts.seconds))
 		exit(EXIT_FAILURE);
 
 	if (opts.exec && opts.fork)
